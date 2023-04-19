d42bc3bc9bfd6431fb1119cc2e6aa20f7d1ab175
NhuDuongHuynh <dnhu877@gmail.com>
2023-04-19 21:02:06 +0700

    Code HowToOrderAService

--- eProject/Controllers/SiteController.cs
+++ eProject/Controllers/SiteController.cs
@@ -13,5 +13,10 @@ namespace eProject.Controllers
         {
             return View();
         }
+
+        public ActionResult HowToOrderAService()
+        {
+            return View();
+        }
     }
 }
