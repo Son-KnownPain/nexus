f782f982ce9669b0535b22819d62bd7f9d12e956
Son-KnownPain <sonhong443@gmail.com>
2023-04-25 21:00:56 +0700

    Make service management page in admin side

--- eProject/Models/Service.cs
+++ eProject/Models/Service.cs
@@ -11,7 +11,10 @@ namespace eProject.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+
     public partial class Service
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -23,11 +26,23 @@ namespace eProject.Models
         }
     
         public int ServiceID { get; set; }
+
+        [DisplayName("Service Name")]
+        [Required(ErrorMessage = "Service name can not empty")]
         public string ServiceName { get; set; }
+
+        [Required(ErrorMessage = "Deposit can not empty")]
         public Nullable<int> Deposit { get; set; }
+
+        [Required(ErrorMessage = "Summary can not empty")]
         public string Summary { get; set; }
+
+        [Required(ErrorMessage = "Description can not empty")]
         public string Description { get; set; }
+
         public string Thumbnail { get; set; }
+
+        public HttpPostedFileBase ThumbnailFile { get; set; }
     
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<Connection> Connections { get; set; }
